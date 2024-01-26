05d65a6d95b1b7cee5265c1c67ddcac05e353824
amitYadavDev <amit8955461017@gmail.com>
2024-01-26 21:04:32 +0530

    get blog details

--- app/src/main/java/amitapps/media/blogapp/screens/details/DetailsViewModel.kt
+++ app/src/main/java/amitapps/media/blogapp/screens/details/DetailsViewModel.kt
@@ -1,9 +1,13 @@
 package amitapps.media.blogapp.screens.details
 
+import amitapps.media.common.Resource
 import amitapps.media.domain.use_cases.GetBlogDetailsUseCase
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 
@@ -13,9 +17,25 @@ class DetailsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
+    private val details = mutableSetOf(BlogDetailsStateHolder())
+
     init {
 
     }
 
-    fun getBlogDetails(id: String)
+    fun getBlogDetails(id: String) {
+        getBlogDetailsUseCase(id).onEach {
+            when(it) {
+                is Resource.Loading -> {
+                    details.value = BlogDetailsStateHolder(isLoading = true)
+                }
+                is Resource.Success -> {
+                    details.value = BlogDetailsStateHolder(data = it.data)
+                }
+                is Resource.Error -> {
+                    details.value = BlogDetailsStateHolder(error = it.message.toString())
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
 }
